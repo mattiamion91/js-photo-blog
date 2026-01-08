af7758787d842891b29b48f4972b6f46744c5d55
Mattia Mion <mattia.mion@gmail.com>
2026-01-08 16:32:23 +0100

    feat: html and css done, writing js ajax call

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,36 @@
 console.log("tutto ok");
+
+//seleziono il contenitore di output
+
+const outputCont = document.getElementById("container")
+console.log(outputCont);
+
+//riferimento ad endpoint
+
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
+
+//creo la chiamata ajax all'endpoint
+
+axios.get(endpoint)
+.then(response => {
+    //recupero array di oggetti dall'api
+    const cards = response.data;
+    console.log(cards);
+
+    //cariabile di accumulo stringa output
+    let cardsOut = ""
+
+    //ciclo array recuperato da api e estrarre informazioni
+    cards.forEach(card => {
+
+        //destrutturo
+        const {date, title, url} = card
+        console.log("il titolo é:", title, "la data é:", date, "l'url é: ");
+        
+        
+    });
+
+
+}
+
+)
