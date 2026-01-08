57790a1949543047494741d4c23c25cad2cf17b6
Mattia Mion <mattia.mion@gmail.com>
2026-01-08 16:50:59 +0100

    feat: adding innerHTML

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
-console.log("tutto ok");
+//console.log("tutto ok");
 
 //seleziono il contenitore di output
 
 const outputCont = document.getElementById("container")
-console.log(outputCont);
+//console.log(outputCont);
 
 //riferimento ad endpoint
 
@@ -12,25 +12,49 @@ const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
 //creo la chiamata ajax all'endpoint
 
 axios.get(endpoint)
-.then(response => {
-    //recupero array di oggetti dall'api
-    const cards = response.data;
-    console.log(cards);
+    .then(response => {
+        //recupero array di oggetti dall'api
+        const cards = response.data;
+        //console.log(cards);
 
-    //cariabile di accumulo stringa output
-    let cardsOut = ""
+        //cariabile di accumulo stringa output
+        let cardsOut = ""
 
-    //ciclo array recuperato da api e estrarre informazioni
-    cards.forEach(card => {
+        //ciclo array recuperato da api e estrarre informazioni
+        cards.forEach(card => {
 
-        //destrutturo
-        const {date, title, url} = card
-        console.log("il titolo é:", title, "la data é:", date, "l'url é: ");
-        
-        
-    });
+            //destrutturo
+            const { date, title, url } = card
+            //console.log("il titolo é:", title, "la data é:", date, "l'url é:", url);
+
+            //valorizzio la variabile di accumulo di output (ad ogni giro del ciclo aggiungo una card fino a raggiungere 6)
+
+            cardsOut += `
+        <figure class="card">
+            <img src="${url}" alt="${title}" class="main-img">
+            <img src="./imgs/pin.svg" alt="pin-absolute" class="pin-img">
+            <figcaption>${title}<br>${date}</figcaption>
+        </figure>
+        `
+            //console.log(cardsOut);
+
+        });
 
 
-}
+        //inserisco in pagina le card accumulate
 
-)
+        outputCont.innerHTML = cardsOut;
+
+    }
+
+    )
+    .catch(error => {
+        //codice da eseguire in caso di errore
+        console.error(error.message);
+    })
+
+    .finally(() => {
+        //codice da eseguire a prescindere dall'esito
+        //esempio: si puo fermare un loader
+        console.log('tutto ok, fine della chiamata');
+    })
